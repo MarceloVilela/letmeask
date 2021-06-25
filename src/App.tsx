e98aacff72eab3a1f48903f9ab7a331ccae558af
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-06-25 19:44:01 -0300

    dia2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
+  
   return (
-    <Button />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
