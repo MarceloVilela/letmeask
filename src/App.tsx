99207fd04886ba59a1b15654fed80c4efb83bdeb
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-06-25 19:50:43 -0300

    dia4

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/rooms/new" component={NewRoom} exact />
         <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </AuthContextProvider>
     </BrowserRouter>
   );
