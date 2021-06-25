0af4590afbf0135ef8fe3b26dbf53b3f99e08003
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-06-25 19:48:01 -0300

    dia3

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { AuthContextProvider } from './contexts/AuthContext';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Route path="/" component={Home} exact />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/new" component={NewRoom} exact />
+        <Route path="/rooms/:id" component={Room} />
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { database } from '../services/firebase';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
-import '../styles/auth.scss';
 
+import '../styles/auth.scss';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -11,13 +13,31 @@ import googleIconImg from '../assets/images/google-icon.svg';
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
-    if(!user){
+    if (!user) {
       await signInWithGoogle();
     }
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`/rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert(`Room does not exists: ${roomCode}`);
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -37,10 +57,12 @@ export function Home() {
           </button>
           <div className="separator">ou entre em uma sala</div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
